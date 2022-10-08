01d4a346d2a43aa5224e20d23a73418424af9afa
nairo-mudumane <nairomudumane@outlook.com>
2022-10-08 08:44:19 +0200

    header component

---
+++ components/header/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+import { StyledHeader } from "./styles";
+
+export function Header() {
+  return (
+    <StyledHeader>
+      <div className="logo">
+        <Image src="/images/logo.png" alt="logo" width="100%" height="60px" />
+      </div>
+
+      <p>Find the perfect teacher!</p>
+    </StyledHeader>
+  );
+}
---
+++ components/header/styles.tsx
@@ -0,0 +1,10 @@
+import { styled } from "@mui/material";
+
+export const StyledHeader = styled("header")`
+  width: 260px;
+  margin: 40px auto;
+
+  .logo {
+    width: 100%;
+  }
+`;
