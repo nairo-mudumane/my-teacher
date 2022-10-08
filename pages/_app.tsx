d171b5eb9f29ca5f730040d271d4c8ce48a58fff
nairo-mudumane <nairomudumane@outlook.com>
2022-10-08 08:50:12 +0200

    theme config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Header } from "../components";
+import { ThemeProvider } from "@mui/material";
+import THEME from "../themes/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={THEME}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
